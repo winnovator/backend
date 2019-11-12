6b269ba61474cb52671a3ca3ac60ed2aed285da1
Marc Hoeve <mrhoeve@gmail.com>
2019-11-12 21:23:38 +0100

    Code refactoring for new Helper class

---
+++ WInnovator/Helper/DefaultUsersAndRoles.cs
@@ -0,0 +1,35 @@
+﻿namespace WInnovator.Helper
+{
+    public static class DefaultUsersAndRoles
+    {
+        public static string[] getRoles()
+        {
+            return new string[] {
+                "Administrator",
+                "Facilitator",
+                "Gebruiker",
+                "FrontendApp"
+            };
+        }
+
+        public static UserData[] getDefaultUsers()
+        {
+            return new UserData[]
+            {
+                new UserData() { email="winnovator@windesheim.nl", password="Welkom@01", defaultRoles=new string[] { "Administrator", "Facilitator", "Gebruiker" } },
+                new UserData() { email="maarten.groensmit@windesheim.nl", password="Welkom@01", defaultRoles=new string[] { "Administrator", "Facilitator", "Gebruiker" } },
+                new UserData() { email="marc.hoeve@windesheim.nl", password="Welkom@01", defaultRoles=new string[] { "Administrator", "Facilitator", "Gebruiker" } },
+                new UserData() { email="niek.pruntel@windesheim.nl", password="Welkom@01", defaultRoles=new string[] { "Administrator", "Facilitator", "Gebruiker" } },
+                new UserData() { email="gert.stoevelaar@windesheim.nl", password="Welkom@01", defaultRoles=new string[] { "Administrator", "Facilitator", "Gebruiker" } },
+            };
+        }
+    }
+
+    public class UserData
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string[] defaultRoles { get; set; }
+    }
+
+}
